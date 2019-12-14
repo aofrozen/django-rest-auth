13d94bacefd1bfbb370b478974ba788a897aeb89
aofrozen <scvjustin@gmail.com>
2019-12-14 01:12:25 -0800

    Update views.py

--- rest_auth/registration/views.py
+++ rest_auth/registration/views.py
@@ -69,15 +69,15 @@ class RegisterView(CreateAPIView):
             'remoteip': get_client_ip(request),  # Optional
             }
         )
-      if r.json()['success']:
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(self.get_response_data(user),
-                        status=status.HTTP_201_CREATED,
-                        headers=headers)
-      return Response(data={'error': 'ReCAPTCHA not verified.'}, status=status.HTTP_406_NOT_ACCEPTABLE, headers=headers)
+        if r.json()['success']:
+          serializer = self.get_serializer(data=request.data)
+          serializer.is_valid(raise_exception=True)
+          user = self.perform_create(serializer)
+          headers = self.get_success_headers(serializer.data)
+          return Response(self.get_response_data(user),
+                          status=status.HTTP_201_CREATED,
+                          headers=headers)
+        return Response(data={'error': 'ReCAPTCHA not verified.'}, status=status.HTTP_406_NOT_ACCEPTABLE, headers=headers)
 
     def perform_create(self, serializer):
         user = serializer.save(self.request)
